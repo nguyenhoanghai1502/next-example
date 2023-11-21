63289b994075af8cb4a4950962d19d11f31f7d77
Nguyễn Hoàng Hải <59238243+nguyenhoanghai1502@users.noreply.github.com>
2023-11-21 14:59:47 +0700

    hai fix deploy

--- app/dashboard/invoices/[id]/divide-profit/page.tsx
+++ app/dashboard/invoices/[id]/divide-profit/page.tsx
@@ -1,11 +1,9 @@
 import Form from '@/app/ui/invoices/divide-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchCustomers } from '@/app/lib/data';
 import { api } from '@/app/lib/axios';
  
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
-//   const customers = await fetchCustomers();
     const [listUsers, profit, profit_list] = await Promise.all([
         api('users/list-users/'),
         api(`profits/detail/${id}`, 'GET', null, {id:id}),
