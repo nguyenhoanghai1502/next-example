040ffd9dae8971f18726a0aa62df58aaa1c5f176
Nguyễn Hoàng Hải <59238243+nguyenhoanghai1502@users.noreply.github.com>
2023-11-29 15:44:04 +0700

    hai fix image

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
     images: {
         remotePatterns: [
           {
-            protocol: 'http',
-            hostname: '127.0.0.1',
+            protocol: 'https',
+            hostname: 'haideptrai.pythonanywhere.com/',
             port: '8000',
           },
         ],
