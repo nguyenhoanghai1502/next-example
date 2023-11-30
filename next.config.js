29de055e9045d2934d3bdbdd297f92704014effe
Nguyễn Hoàng Hải <59238243+nguyenhoanghai1502@users.noreply.github.com>
2023-11-30 17:14:28 +0700

    hai add create user

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
     images: {
         remotePatterns: [
           {
-            protocol: 'http',
-            hostname: '127.0.0.1',
-            port: '8000',
+            protocol: 'https',
+            hostname: 'haideptrai.pythonanywhere.com',
+            port: '',
           },
         ],
       },
